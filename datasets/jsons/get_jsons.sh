7112ff3c7e26db1e5abdce7e1f8681fee92dfc3f
Çağan Selim Çoban <caganselim@gmail.com>
2022-03-14 12:48:26 +0300

    Edited dropbox link.

--- datasets/jsons/get_jsons.sh
+++ datasets/jsons/get_jsons.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/j53xxzcbmna6np1/jsons.zip?dl=0 -O jsons.zip
+wget https://www.dropbox.com/s/j53xxzcbmna6np1/jsons.zip?dl=1 -O jsons.zip
 unzip jsons.zip
 rm jsons.zip
